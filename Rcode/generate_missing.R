2ad724951b7f0528b54900dffd32d837398fb283
Anogio <antoine.ogier@gmail.com>
2018-05-28 16:17:16 +0100

    Add simple MCAR

---
+++ Rcode/generate_missing.R
@@ -0,0 +1,14 @@
+
+
+MCAR <- function(X, miss_prop=0.05){
+  print(paste('Adding ', miss_prop*100, '% missing observations completely at random.', sep=''))
+  n = nrow(X)
+  m = ncol(X)
+  missing = matrix(runif(n*m), nrow=n, ncol=m) < miss_prop
+  X[missing] = NA
+  
+  missing = sum(colSums(is.na(X)))
+  prop_missing = missing/(n*m)
+  print(paste('New proportion of missing data: ', prop_missing*100, '%.', sep=''))
+  return(X)
+}
