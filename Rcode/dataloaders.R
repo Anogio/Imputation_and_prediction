84784fb8f44391393bb79702b0a158d64a989da1
Anogio <antoine.ogier@gmail.com>
2018-05-28 14:49:57 +0100

    Refactor loader for generality

--- Rcode/dataloaders.R
+++ Rcode/dataloaders.R
@@ -1,53 +1,67 @@
 library(tidyverse)
 
-summarise_xy <-  function(X,y){
-  missing = sum(colSums(is.na(X)))
-  missing_prop = missing/(nrow(X)*ncol(X))
-  print(paste(missing, ' (', missing_prop*100, '%) missing observations.', sep=''))
-  print(paste('Done (', nrow(X), ' rows, ', ncol(X), ' columns).', sep=''))
+#############################
+# Information on each dataset, used for automatic loading
+data_folder = '../Data/'
+
+files = list(
+  iris = 'iris.csv',
+  titanic = 'titanic.csv'
+)
+y_columns = list(
+  iris = 5,
+  titanic = 2
+)
+cat_columns = list(
+  iris = c(),
+  titanic = c(3, 5, 12)
+)
+num_columns = list(
+  iris = 1:4,
+  titanic = c(6:8, 10)
+)
+
+##################
+# Helper functions
+
+summarise_x <-  function(X_num, X_cat){
+  # Prints diagnostic messages on the imported data
+  missing_num = sum(colSums(is.na(X_num)))
+  missing_prop_num = missing_num/(nrow(X_num)*ncol(X_num))
+  missing_cat = sum(colSums(is.na(X_cat)))
+  missing_prop_cat = missing_cat/(nrow(X_cat)*ncol(X_cat))
+  
+  print(paste(
+    'Imported ', ncol(X_num), ' numerical variables (', missing_prop_num*100, '% missing entries), and ',
+    ncol(X_cat), ' categorical variables (', missing_prop_cat*100, '% missing entries).'
+  ))
+  print(paste('Done (', nrow(X_num), ' rows).', sep=''))
 }
 
-iris_loader <- function(datatypes='numeric'){
-  print(paste('Loading iris data (selected data type: ', datatypes, ') ...', sep=''))
-  data(iris)
-  if(datatypes=='numeric' | datatypes=='all'){
-    X=iris[,1:4]
-    y=iris[,5]
-  }
-  else{
-    stop('Iris only has numeric values.')
+###############################
+# Main loader
+loader <- function(dataset){
+  print(paste('Loading ',  dataset, ' data...', sep=''))
+  
+  dat = read.csv(paste(data_folder,files[dataset], sep=''))
+  ycol = y_columns[[dataset]]
+  numcols = num_columns[[dataset]]
+  catcols = cat_columns[[dataset]]
+  y = as.factor(dat[, ycol])
+  X_num = dat[, numcols]
+  X_cat = dat[, catcols]
+  
+  if (ncol(X_cat)>0){
+    for(i in 1:ncol(X_cat)){
+      X_cat[,i] = as.factor(X_cat[,i])
+    }
   }
   
-  summarise_xy(X,y)
-  return(list(X=X, y=y))
+  summarise_x(X_num, X_cat)
+  return(list(X_numeric=X_num, X_category=X_cat, y=y))
 }
 
-titanic_loader <- function(datatypes='all'){
-  print(paste('Loading titanic data (selected data type: ', datatypes, ') ...', sep=''))
-  
-  tit = read.csv('../Data/titanic.csv')
-  tit$Pclass = as.factor(tit$Pclass)
-  if(datatypes=='all'){
-    X=tit[,c(3, 5:8,10,12)]
-    y=tit[,2]
-  }
-  else if(datatypes=='category'){
-    X=tit[,c(3, 5, 12)]
-    y=tit[,2]
-  }
-  else if(datatypes=='numeric'){
-    X=tit[,c(6:8, 11)]
-    y=tit[,2]
-  }
-  else{
-    stop('Wrong data type specified: must be "all", "numeric" or "category".')
-  }
-  
-  summarise_xy(X,y)
-  return(list(X=X, y=y))
-}
-
-trauma_loader <- function(){
-  # TODO : load trauma data using Wei's code
-}
-
+###############
+# Example
+tit = loader('titanic')
+ir = loader('iris')
--- Rcode/main.R
+++ Rcode/main.R
@@ -3,5 +3,6 @@ library(tidyverse)
 
 # Custom imports
 source('dataloaders.R')
+source('generate_missing.R')
 source('impuation_methods.R')
 source('prediction_methods.R')
