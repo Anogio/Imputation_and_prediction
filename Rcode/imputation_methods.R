f0bcd7521d1c676008e3e6d96193c78133fdf808
Anogio <antoine.ogier@gmail.com>
2018-05-28 16:38:16 +0100

    Add status messages

--- Rcode/imputation_methods.R
+++ Rcode/imputation_methods.R
@@ -2,11 +2,13 @@ library(tidyverse)
 library(mice)
 
 mice_imp <- function(X, m=5){
-  imp = mice(X, m=m)
+  print(paste('Performing', m, 'imputations with MICE...'))
+  imp = mice(X, m=m, printFlag = F)
   res = list()
   for(i in 1:m){
     res[[i]] = complete(imp,i)
   }
+  print('Done')
   return(res)
 }
 
