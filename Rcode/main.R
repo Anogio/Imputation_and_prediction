cc0b010b50e59ff418cd8e337b80afab82304d15
Anogio <antoine.ogier@gmail.com>
2018-05-28 16:40:21 +0100

    Example main workflow

--- Rcode/main.R
+++ Rcode/main.R
@@ -6,3 +6,17 @@ source('dataloaders.R')
 source('generate_missing.R')
 source('imputation_methods.R')
 source('prediction_methods.R')
+
+seed = 42
+
+dataset = 'iris'
+
+dat = loader(dataset)
+X = cbind(dat$X_numeric, dat$X_category)
+y=dat$y
+
+X_miss = MCAR(X, 0.05)
+
+X_imputed = mice_imp(X_miss, m=5)
+
+predictions = multiple_prediction(X_imputed, y, pred_method = 'rf', train_size = 0.5, seed=seed)
