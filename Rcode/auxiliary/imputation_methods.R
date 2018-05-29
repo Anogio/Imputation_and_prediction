bc73663eb12553000d9b4877f01b1f666e0d8460
Anogio <antoine.ogier@gmail.com>
2018-05-29 16:16:25 +0100

    UNTESTED add Amelia, mi and cat imputation

--- Rcode/auxiliary/imputation_methods.R
+++ Rcode/auxiliary/imputation_methods.R
@@ -1,5 +1,9 @@
 library(tidyverse)
+library(mi)
 library(mice)
+library(Amelia)
+library(cat)
+
 
 mice_imp <- function(X, m=5){
   print(paste('Performing', m, 'imputations with MICE...'))
@@ -12,4 +16,35 @@ mice_imp <- function(X, m=5){
   return(res)
 }
 
+amelia_imp <- function(X, m=5){
+  print(paste('Performing', m, 'imputations with Amelia...'))
+  imp = amelia(X, m=m, ncpus = 4, noms=which(sapply(X,is.factor)))$imputations
+  print('Done.')
+  return(imp)
+}
 
+mi_imp <- function(X, m=5){
+  print(paste('Performing', m, 'imputations with mi...'))
+  X_m = missing_data.frame(X)
+  X_m = mi(X)
+  X_m = complete(X_m, m=m)
+  print('Done')
+  return(X_m)
+}
+
+cat_imp <- function(X, m=5){
+  print(paste('Performing', m, 'imputations with cat...'))
+  if(!all(sapply(X,is.factor))){
+    stop("All variables must be categorical")
+  }
+  rngseed()
+  
+  X = as.matrix(as.data.frame(lapply(X,as.numeric)))
+  X_p = prelim.cat(X)
+  theta = em.cat(X_p)
+  imp = list()
+  for(i in 1:m){
+    imp[[i]] = imp.cat(X_p, theta)
+  }
+  return(imp)
+}
