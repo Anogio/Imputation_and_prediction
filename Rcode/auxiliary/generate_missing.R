b325bad6d6a9129e58d5c3cac6d6d33cdc167a65
Anogio <antoine.ogier@gmail.com>
2018-05-29 11:09:18 +0100

    Bug fix

--- Rcode/auxiliary/generate_missing.R
+++ Rcode/auxiliary/generate_missing.R
@@ -4,7 +4,7 @@ MCAR <- function(X, miss_prop=0.05){
   print(paste('Adding ', miss_prop*100, '% missing observations completely at random.', sep=''))
   n = nrow(X)
   m = ncol(X)
-  missing = matrix(runif(n*m), nrow=n, ncol=m) < miss_prop
+  missing = matrix(runif(n*m), nrow=n, ncol=m) <= miss_prop
   X[missing] = NA
   
   missing = sum(colSums(is.na(X)))
